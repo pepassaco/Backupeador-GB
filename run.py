e55c1e3f89fbb7497ba95a404009fbf60aad41e5
EA1FID <quitatemedeadiante@gmail.com>
2025-01-07 00:58:51 +0100

    Added start text

--- run.py
+++ run.py
@@ -1,40 +1,43 @@
 import RPi.GPIO as GPIO
 import time
 from backupeador import backupeador
-
+from localConfig import pinGB, pinGBA
 
 def detectedGBA():
-	print("GBA game selected")
-	bckp = backupeador()
-	bckp.backupGBA()
+        print("GBA game selected")
+        bckp = backupeador()
+        bckp.backupGBA()
 
 def detectedGB():
-	print("GBC game selected")
-	bckp = backupeador()
-	bckp.backupGB()
+        print("GBC game selected")
+        bckp = backupeador()
+        bckp.backupGB()
 
 def main():
 
-	while(True):
-		try:
-			if not GPIO.input(23):
-				detectedGB()
-			elif not GPIO.input(17):
-				detectedGBA()
-			else:
-				time.sleep(0.01)                    
-		except KeyboardInterrupt:
-			GPIO.cleanup()       # clean up GPIO on CTRL+C exit
-			exit()
-	GPIO.cleanup()           # clean up GPIO on normal exit
+        while(True):
+                try:
+                        if GPIO.input(pinGB):
+                                detectedGB()
+                        elif GPIO.input(pinGBA):
+                                detectedGBA()
+                        else:
+                                time.sleep(0.05)
+                except KeyboardInterrupt:
+                        GPIO.cleanup()       # clean up GPIO on CTRL+C exit
+                        exit()
+        GPIO.cleanup()           # clean up GPIO on normal exit
 
 
 
 
 if __name__ == '__main__':
 
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	print("Starting Backupeador. Project by github.com/pepassaco")
+ 	print("Have fun! :)")
+	
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(pinGB, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(pinGBA, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-	main()
+        main()
